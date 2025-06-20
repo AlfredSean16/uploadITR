68145275d11afc61ab11920815c921a43df66dd3
Alfred Sean <alfredseanfermin@gmail.com>
2025-06-20 12:04:00 +0800

    Initial commit

---
+++ src/main/java/com/metrobank/uploadITR/UploadItrApplication.java
@@ -0,0 +1,13 @@
+package com.metrobank.uploadITR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadItrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadItrApplication.class, args);
+	}
+
+}
